fd82c7f0eed5288442856d4f6c0308a27634a47b
vagner bohm <vagnerpelais@gmail.com>
2023-07-24 14:32:21 -0300

    added .env example and fix mongodb conn

--- src/main/java/com/vagnerbohm/movies/MoviesApplication.java
+++ src/main/java/com/vagnerbohm/movies/MoviesApplication.java
@@ -1,9 +1,12 @@
 package com.vagnerbohm.movies;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class MoviesApplication {
 
 	public static void main(String[] args) {
