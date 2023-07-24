6dbfe98a7d64e7c7832bfe3d295d113536725911
vagner bohm <vagnerpelais@gmail.com>
2023-07-24 15:07:18 -0300

    added reviews and movie documents

---
+++ src/main/java/com/vagnerbohm/movies/document/MovieDocument.java
@@ -0,0 +1,29 @@
+package com.vagnerbohm.movies.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "movies")
+public class MovieDocument {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<ReviewDocument> reviewIds;
+}
---
+++ src/main/java/com/vagnerbohm/movies/document/ReviewDocument.java
@@ -0,0 +1,18 @@
+package com.vagnerbohm.movies.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "reviews")
+public class ReviewDocument {
+    @Id
+    private ObjectId id;
+    private String body;
+}
